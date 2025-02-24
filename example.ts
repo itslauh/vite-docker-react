b344a6488b46a45f8e0e2114d37bb48f57c39b7e
Laurens <wezellaurens@gmail.com>
2025-02-24 08:44:15 +0100

    add example TypeScript types and constants for various data types

---
+++ example.ts
@@ -0,0 +1,23 @@
+const textString = "textString";
+const textArray = ["textArray"];
+const textObject = { textObject: "textObject" };
+
+const number = 1;
+const numberArray = [1];
+const numberObject = { numberObject: 1 };
+
+const boolean = true;
+const booleanArray = [true];
+const booleanObject = { booleanObject: true };
+
+const TypedTextString: string = "textString";
+const TypedTextArray: string[] = ["textArray"];
+const TypedTextObject: { textObject: string } = { textObject: "textObject" };
+
+const TypedNumber: number = 1;
+const TypedNumberArray: number[] = [1];
+const TypedNumberObject: { numberObject: number } = { numberObject: 1 };
+
+const TypedBoolean: boolean = true;
+const TypedBooleanArray: boolean[] = [true];
+const TypedBooleanObject: { booleanObject: boolean } = { booleanObject: true };
