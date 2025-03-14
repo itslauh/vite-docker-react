6f0d500c5b97759384ffdb11958d7025d705fe69
Laurens <wezellaurens@gmail.com>
2025-03-14 17:47:24 +0100

    refactor App component to use Material-UI and remove unused Example component and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
-import { Example } from './components/Example';
+import { Button, Stack, Typography } from '@mui/material';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Stack   direction="column"
+  spacing={2}
+  sx={{
+    justifyContent: "center",
+    alignItems: "center",
+  }}>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -17,19 +22,15 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+      <Typography variant={"h3"}>Vite + React</Typography>
+        <Button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
+        </Button>
+        <Typography variant={"h4"}>Edit src/App.tsx and save to test HMR</Typography>
+      <Typography>
         Click on the Vite and React logos to learn more
-      </p>
-      <Example value={"hi from App.tsx"}/>
+      </Typography>
+    </Stack>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,35 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#757ce8',
+      main: '#663399',
+      dark: '#002884',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ff7961',
+      main: '#f44336',
+      dark: '#ba000d',
+      contrastText: '#000',
+    },
+  },
+});
+
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <ThemeProvider theme={theme}>
     <App />
+    </ThemeProvider>
   </StrictMode>,
 )
